ff5f9c1cea0f3f6151208809f90b130eb33b564a
Julian Llanten <hugojulians@gmail.com>
2025-07-19 14:42:34 -0500

    fix: agregar soporte para sitemap en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,22 @@ import tailwindcss from "@tailwindcss/vite";
 
 import robotsTxt from "astro-robots-txt"
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    robotsTxt({
-      policy: [
-        { userAgent: '*', allow: '/' },
-        { userAgent: 'Googlebot', allow: '/' },
-        { userAgent: 'Bingbot', allow: '/' },
-      ],
-      sitemap: true,
-      host: 'https://julianll.dev/',
-      additionalSitemaps: [
-        'https://julianll.dev/sitemap.xml'
-      ],
-    }),
-  ],
+  integrations: [robotsTxt({
+    policy: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Googlebot', allow: '/' },
+      { userAgent: 'Bingbot', allow: '/' },
+    ],
+    sitemap: true,
+    host: 'https://julianll.dev/',
+    additionalSitemaps: [
+      'https://julianll.dev/sitemap.xml'
+    ],
+  }), sitemap()],
   site: 'https://julianll.dev/',
    vite: {
     plugins: [tailwindcss()],
