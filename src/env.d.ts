56d1213e76cd481da68b68cbd37651ef0619f565
Julian Llanten <hugojulians@gmail.com>
2026-05-26 20:57:11 -0500

    Add: update packages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Declaraciones para paquetes sin tipos propios
+declare module '@fontsource-variable/onest'
